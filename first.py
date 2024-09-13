02fb1ce6bf5d1cb8744fa55bea0bd38be1ed9cfd
neev151 <mehtaneev991@gmail.com>
2024-09-13 04:34:21 -0700

    new paragraph added

--- first.py
+++ first.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("My name is Mehta Neev")
+# print("Hello World")
+# print("My name is Mehta Neev")
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
